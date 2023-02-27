aa9225cce9b80e80af081f33f4660d464e50b108
Serguha667 <sanat.200@mail.ru>
2023-02-27 12:09:46 +0600

    qwerwqer

---
+++ midterm/blogs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Bloq (models.Model):
+    name = models. CharField(max_Length=255, nUll=False)
+    description = models. CharField (max_length=255, null=True)
+    owner = models. CharField(max_Length=255, null=False)
+
+    class Meta:
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        return 'ID: {}, Name: {}'.format(self.id, self.name)
+
