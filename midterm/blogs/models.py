46be6538c815307a952d503ebd0c1756efb611f4
Serguha667 <sanat.200@mail.ru>
2023-02-27 12:23:20 +0600

    sadfasdfsadf

--- midterm/blogs/models.py
+++ midterm/blogs/models.py
@@ -1,15 +1,78 @@
 from django.db import models
+import requests
+import requests
+import requests
 
-# Create your models here.
-class Bloq (models.Model):
-    title = models. CharField(max_Length=255, nUll=False)
-    description = models. CharField (max_length=255, null=True)
-    owner = models. CharField(max_Length=255, null=False)
 
-    class Meta:
-        verbose_name = 'Blog'
-        verbose_name_plural = 'Blogs'
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    owner = models.CharField(max_length=255)
 
-    def __str__(self):
-        return 'ID: {}, Name: {}'.format(self.id, self.name)
 
+url = "http://yourwebsite.com/api/blogs"
+response = requests.get(url)
+
+if response.status_code == 200:
+    blogs = response.json()
+    print(blogs)
+else:
+    print("Ошибка при получении списка блогов")
+    var = [
+        {
+            "id": 1,
+            "title": "Blog 1 1",
+            "description": "Blog 2 2",
+            "owner": "Blog 3 3"
+        },
+        {
+            "id": 2,
+            "title": "Blog 1 1 1",
+            "description": "Blog 2 2 2",
+            "owner": "Blog 3 3 3"
+        },
+        {
+            "id": 3,
+            "title": "Blog 1",
+            "description": "Blog 2",
+            "owner": "Blog 3"
+        }
+    ]
+
+url = "http://yourwebsite.com/api/blogs"
+
+new_blog = {
+    "title": "Blog 1",
+    "description": "Blog 2",
+    "owner": "Blog 3"
+}
+
+response = requests.post(url, json=new_blog)
+
+if response.status_code == 201:
+    created_blog = response.json()
+    print(created_blog)
+else:
+    print("Ошибка")
+var = {
+    "id": 4,
+    "title": "Blog 1",
+    "description": "Blog 2",
+    "owner": "Blog 3"
+}
+
+url = "http://yourwebsite.com/api/blogs/4"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    blog = response.json()
+    print(blog)
+else:
+    print("Ошибка при получении блога")
+    var = {
+        "id": 4,
+        "title": "Blog 1",
+        "description": "Blog 2",
+        "owner": "Blog 3"
+    }
